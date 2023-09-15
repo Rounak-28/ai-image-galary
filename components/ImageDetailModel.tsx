ee6e1e8c9ce5dbc9252941c9e46f7a5eeeeaac5d
Rounak-28 <rounakkumbhakar2004@gmail.com>
2023-09-15 22:39:50 +0530

    some more work

--- components/ImageDetailModel.tsx
+++ components/ImageDetailModel.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const ImageDetailModel = ({ setIsModelOpen }: any) => {
   return (
     <div className="w-screen h-screen absolute top-0 left-0 flex justify-center items-center z-[999]">
-      <div className="w-2/3 h-3/4 bg-[#27272a] relative">
+      <div className="w-2/3 h-3/4 bg-[#27272a] rounded-md border-[1px] border-gray-300 relative">
         <button
           className="text-2xl absolute top-4 left-4"
           onClick={() => setIsModelOpen(false)}
