621a9fecd5bc32dc4005ccab9670a17176819aa7
Rounak-28 <rounakkumbhakar2004@gmail.com>
2023-09-23 20:03:54 +0530

    loading anim on geenrate, plus mocking real api call for now

--- app/generate/page.tsx
+++ app/generate/page.tsx
@@ -2,14 +2,17 @@
 
 import Image from "next/image";
 import React, { useState } from "react";
+import ReactLoading from "react-loading";
 
 const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
 
 const Page = () => {
   const [inputText, setInputText] = useState("");
   const [prediction, setPrediction]: any = useState(null);
+  const [isloading, setIsloading] = useState(false);
 
   const handleGenerate = async () => {
+    setIsloading(true);
     let response = await fetch("/api/generate", {
       method: "POST",
       headers: {
@@ -30,53 +33,44 @@ const Page = () => {
       const response = await fetch("/api/generate/" + prediction.id);
       prediction = await response.json();
       if (response.status !== 200) {
-        console.log("error bruhhh...");
+        setIsloading(false);
+        console.error("error bruhhh...");
         return;
       }
       setPrediction(prediction);
     }
+    setIsloading(false);
+  };
 
-    // console.log(prediction);
-
-    // {
-    //   "id": "bbzjfcjbjfmjl7p5vjzddf7vni",
-    //   "version": "ac732df83cea7fff18b8472768c88ad041fa750ff7682a21affe81863cbe77e4",
-    //   "input": {
-    //     "prompt": "an astronaut on the sun"
-    //   },
-    //   "logs": "",
-    //   "error": null,
-    //   "status": "starting",
-    //   "created_at": "2023-09-22T15:17:55.681027073Z",
-    //   "urls": {
-    //     "cancel": "https://api.replicate.com/v1/predictions/bbzjfcjbjfmjl7p5vjzddf7vni/cancel",
-    //     "get": "https://api.replicate.com/v1/predictions/bbzjfcjbjfmjl7p5vjzddf7vni"
-    //   }
-    // }
-
-    // {
-    //   "id": "73hnl4bby5rwn5rldf7xpxmsqu",
-    //   "version": "ac732df83cea7fff18b8472768c88ad041fa750ff7682a21affe81863cbe77e4",
-    //   "input": {
-    //     "prompt": "a cat on the moon"
-    //   },
-    //   "logs": "pretty long, removed",
-    //   "output": [
-    //     "https://pbxt.replicate.delivery/8K4oUDcC6fQ4SKd0ojkOeRXf862eS4gFHvheGNTMYZe10ntZE/out-0.png"
-    //   ],
-    //   "error": null,
-    //   "status": "succeeded",
-    //   "created_at": "2023-09-22T15:41:59.764346Z",
-    //   "started_at": "2023-09-22T15:42:06.86155Z",
-    //   "completed_at": "2023-09-22T15:42:12.515937Z",
-    //   "metrics": {
-    //     "predict_time": 5.654387
-    //   },
-    //   "urls": {
-    //     "cancel": "https://api.replicate.com/v1/predictions/73hnl4bby5rwn5rldf7xpxmsqu/cancel",
-    //     "get": "https://api.replicate.com/v1/predictions/73hnl4bby5rwn5rldf7xpxmsqu"
-    //   }
-    // }
+  const handleGenerateTest = async () => {
+    setIsloading(true);
+    await sleep(2000);
+    setPrediction({
+      id: "73hnl4bby5rwn5rldf7xpxmsqu",
+      version:
+        "ac732df83cea7fff18b8472768c88ad041fa750ff7682a21affe81863cbe77e4",
+      input: {
+        prompt: "a cat on the moon",
+      },
+      logs: "pretty long, removed",
+      output: [
+        "https://pbxt.replicate.delivery/8K4oUDcC6fQ4SKd0ojkOeRXf862eS4gFHvheGNTMYZe10ntZE/out-0.png",
+      ],
+      error: null,
+      status: "succeeded",
+      created_at: "2023-09-22T15:41:59.764346Z",
+      started_at: "2023-09-22T15:42:06.86155Z",
+      completed_at: "2023-09-22T15:42:12.515937Z",
+      metrics: {
+        predict_time: 5.654387,
+      },
+      urls: {
+        cancel:
+          "https://api.replicate.com/v1/predictions/73hnl4bby5rwn5rldf7xpxmsqu/cancel",
+        get: "https://api.replicate.com/v1/predictions/73hnl4bby5rwn5rldf7xpxmsqu",
+      },
+    });
+    setIsloading(false);
   };
 
   return (
@@ -92,10 +86,12 @@ const Page = () => {
         />
         <button
           className="w-28 h-12 rounded-lg bg-[#352f9b] hover:bg-[#4640bb]"
-          onClick={handleGenerate}
+          // onClick={handleGenerate}
+          onClick={handleGenerateTest} // just for testing purposes without using real api
         >
           Generate
         </button>
+        {isloading && <ReactLoading type="bars" width={150} height={100} />}
         {prediction?.output && (
           <img src={prediction?.output[0]} alt="" className="w-96 h-96" />
         )}
