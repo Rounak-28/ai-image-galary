052f2b1f9e53a7c89228c7517d6b546e9797b316
Rounak-28 <rounakkumbhakar2004@gmail.com>
2023-09-25 18:04:57 +0530

    dont let them generate without signup

--- components/TopNavbar.tsx
+++ components/TopNavbar.tsx
@@ -1,14 +1,19 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
 import LoginBtn from "./LoginBtn";
+import { useSession } from "next-auth/react";
 
 const TopNavbar = () => {
+  const { data: session } = useSession();
+
   return (
     <div className="h-14 bg-[#1b1b1d] text-white flex justify-center items-center space-x-6 sticky top-0 right-0 z-[999]">
       <Link href="/">
         <div className="px-2 py-2 flex justify-center">Home</div>
       </Link>
-      <Link href="/generate">
+      <Link href={session ? "/generate" : "/api/auth/signin"}>
         <div className="px-2 py-2 flex justify-center">Generate</div>
       </Link>
       <LoginBtn />
